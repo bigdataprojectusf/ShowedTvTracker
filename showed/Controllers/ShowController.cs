9e9f02b894cf499b4a6c3f2371bf1d7d4c1ff8c8
wijen1 <wjaugustine@gmail.com>
2015-05-05 14:37:56 +0200

    Early version showed 05-05-2015

---
+++ showed/Controllers/ShowController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TVDBSharp;
+using TVDBSharp.Models;
+
+namespace showed.Controllers
+{
+    public class ShowController : Controller
+    {
+        private TVDB _tvdb;
+
+        public ShowController()
+        {
+            _tvdb = new TVDB("86F59A0BFBA75DB4");
+        }
+
+        // GET: Show
+        //My shows / manage shows page
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Calender view page
+        public ActionResult MyCalender()
+        {
+            return View();
+        }
+
+        //Page returning searchresults
+        [HttpGet]
+        public ActionResult SearchResults()
+        {
+            ViewBag.SearchTerm = "";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SearchResults(string search)
+        {
+            var results = _tvdb.Search(search, 3);
+            IEnumerable<Show> shows = results;
+            return View(shows);
+        }
+
+        public ActionResult Follow()
+        {
+            return View();
+        }
+    }
+}
---
+++ showed/Models/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace showed.Models
+{
+    public class Member
+    {
+        public int MemberId { get; set; }
+        public string Surname { get; set; }
+        public string Lastname { get; set; }
+        public ICollection<int> ShowsIdCollection { get; set; }
+    }
+}
